8f8118be83620c7a10f60fe5e0012974e305e16c
2018507033 <92920720+2018507033@users.noreply.github.com>
2021-11-14 16:56:16 +0300

    Update oop_assingment.py

--- oop_assingment.py
+++ oop_assingment.py
@@ -22,15 +22,34 @@ Sınıf=4
 '''
 
 class urunler:
-def __init__(self,Zekmek,Zyumurta,Zpatates,Zelma,Zun)
-super().__init__()
-self.ekmek = Zekmek
-self.yumurta = Zyumurta
-self.patates = Zpatates
-self.elma = Zelma
-self.un = Zun
+name = ""
+comments = ""
+OTVvergi = 0
+KDVvergi = 0
+ilk_fiyat = 0
+son_fiyat = 0
+def __init__(self,qname,qOTVvergi,qKDVvergi,qilk_fiyat)
+self.name=qname
+self.OTVvergi=qOTVvergi
+self.KDVvergi=qKDVvergi
+self.ilk_fiyat = qilk_fiyat
+self.fiyat()
 
+def fiyat(self)
+tmp = self.ilk_fiyat + (self.ilk_fiyat*self.OTVvergi)
+self.son_fiyat= tmp + (tmp*self.KDVvergi)
 
+def pprint(self):
+    print(f'{self.name}\'ın Fiyatı {self.son_fiyat}TL')
+    ekmek = urunler ("Lavaş",0.1,0.2,200)
+    peynir = urunler("kaşar",0.2,0.3,300)
+    
+   ekmek.pprint()
+
+lavaşın fiyatı 264.0TL
+
+peynir.pprint()
+Kaşarın Fiyatı 468.0TL
     
           
     
@@ -49,10 +68,7 @@ def sepet_fiyati(kar_orani):
     yumurta=urunler('yumurta',5,0.30,0.19)
     toplam=0   
     #-------**-----------
-for toplam in range(6):
-  print(toplam)
-else:
-  print("Bir Hata Meydana geldi")
+
 
     #-------**-----------
     return toplam
